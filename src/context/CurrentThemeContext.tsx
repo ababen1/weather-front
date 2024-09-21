f7a8955f73c7132611e0cc2ab7b2acba9910c223
ababen1 <ben.egg.gov@gmail.com>
2024-09-21 14:35:20 +0300

    saved theme in local storage

--- src/context/CurrentThemeContext.tsx
+++ src/context/CurrentThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { createTheme, Theme, ThemeProvider } from '@mui/material';
 
 const BLACK = '#121212'
@@ -32,33 +32,42 @@ export const lightTheme = createTheme({
 interface ICurrentThemeContext {
     theme: Theme;
     toggleTheme: () => void;
-  }
-  
-  // Create the context
-  export const CurrentThemeContext = createContext<ICurrentThemeContext>({
-    theme: darkTheme, 
-    toggleTheme: () => {},
-  });
-  
-  interface CurrentThemeProviderProps {
+}
+
+// Create the context
+export const CurrentThemeContext = createContext<ICurrentThemeContext>({
+    theme: darkTheme,
+    toggleTheme: () => { },
+});
+
+interface CurrentThemeProviderProps {
     children: ReactNode;
-  }
-  
-  // Create the provider to manage theme state and toggle functionality
-  export const CurrentThemeProvider = ({ children }: CurrentThemeProviderProps) => {
+}
+
+// Create the provider to manage theme state and toggle functionality
+export const CurrentThemeProvider = ({ children }: CurrentThemeProviderProps) => {
+
+    // Load the user's selected theme, if it exists.
+    useEffect(() => {
+        const selectedTheme = localStorage.getItem("theme")
+        if (selectedTheme != null) {
+            setCurrentTheme(selectedTheme === "dark" ? darkTheme : lightTheme)
+        }
+    }, [])
+
     const [currentTheme, setCurrentTheme] = useState<Theme>(darkTheme);
-  
+
     const toggleTheme = () => {
-      setCurrentTheme((prevTheme) =>
-        prevTheme.palette.mode === 'dark' ? lightTheme : darkTheme
-      );
+        const newTheme: Theme = currentTheme.palette.mode === 'dark' ? lightTheme : darkTheme
+        setCurrentTheme(newTheme)
+        localStorage.setItem("theme", newTheme.palette.mode)
     };
-  
+
     return (
-      <CurrentThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
-        <ThemeProvider theme={currentTheme}>
-          {children}
-        </ThemeProvider>
-      </CurrentThemeContext.Provider>
+        <CurrentThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+            <ThemeProvider theme={currentTheme}>
+                {children}
+            </ThemeProvider>
+        </CurrentThemeContext.Provider>
     )
 }
